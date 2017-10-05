37e0d32535f6dd7b5aa99b1bc640dc6ae5998b83
BenjaVR <benjamin.v.renterghem@gmail.com>
2017-10-05 09:49:44 -0500

    update files

--- posh-ssh-list/posh-ssh-list.psm1
+++ posh-ssh-list/posh-ssh-list.psm1
@@ -1,6 +1,18 @@
-<#
-	My Function
-#>
-function Get-Function {
+function Read-SshConfig {
+	param(
+		[string] $SshConfigFilePath = [io.path]::combine($env:USERPROFILE, '.ssh', 'config'),
+		[switch] $ShowFullFile = $false
+	)
 
+	$content = (Get-Content $SshConfigFilePath -Raw | Out-String)	
+
+	Write-Output `n
+	if ($ShowFullFile) {
+		Write-Output $content
+	} else {
+		Write-Output ($content | Select-String '(?<=Host\s).+' -AllMatches |
+			Foreach {$_.Matches} | Foreach {$_.Value})
+	}
+	Write-Output `n
 }
+Export-ModuleMember -Function Read-SshConfig
